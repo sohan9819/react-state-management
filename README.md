9e20b75ee6b48ef78b1c63ceb22ffdb7d5fef70d
Sohan <sohanshetty2001@gmail.com>
2023-02-01 20:36:48 +0530

    Added code exmaples for useState

--- README.md
+++ README.md
@@ -11,3 +11,24 @@
 - Valtio
 - Jotal
 - Redux
+
+## `useState`
+
+- Data types in javascript
+
+| Value Types / Primitive | Reference Types / Non-Primitive |
+| ----------------------- | ------------------------------- |
+| Number                  | Object                          |
+| String                  | Array                           |
+| Boolean                 | Function                        |
+| Symbol                  |                                 |
+| Undefined               |                                 |
+| Null                    |                                 |
+
+[Understanding difference between _pass by value_ and _pass by reference_](https://https://www.scaler.com/topics/javascript/pass-by-value-and-pass-by-reference/)
+
+- `useState` can also take a function as a Starting point
+
+```javascript
+const [count, setCount] = useState(() => 10);
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function Counter() {
+  const [count, setCount] = useState(10);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <button
+      onClick={() => {
+        setCount((prev) => prev + 1);
+      }}
+    >
+      Count = {count}
+    </button>
+  );
 }
 
-export default App
+function NameList() {
+  const [nameList, setNameList] = useState(['Sohan', 'Snicker', 'Vicky']);
+
+  const [input, setInput] = useState('');
+
+  return (
+    <>
+      <ul>
+        {nameList.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      <input
+        type='text'
+        name='name'
+        id='name'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button
+        type='submit'
+        onClick={() => {
+          if (input.trim()) {
+            setNameList([...nameList, input]);
+            setInput('');
+          }
+        }}
+      >
+        Add Name
+      </button>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div className='App'>
+      <Counter />
+      <Counter />
+      <Counter />
+      <NameList />
+    </div>
+  );
+}
+
+export default App;
