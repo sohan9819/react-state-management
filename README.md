7d879a8db5d7287f309b6ec5791b9af0caef6a56
Sohan <sohanshetty2001@gmail.com>
2023-02-02 21:27:46 +0530

    Added code examples for useCallback

--- README.md
+++ README.md
@@ -11,3 +11,19 @@
 - Valtio
 - Jotal
 - Redux
+
+## `useCallback`
+
+[React Docs for useCallback](https://reactjs.org/docs/hooks-reference.html#usecallback)
+
+- It's same as `useMemo()` , but it returns a memoized callback.
+- Syntax
+
+```javascript
+// useCallback(() => {
+//   first;
+// }, [second]);
+
+const sortFunc = useCallback((a, b) => a.localeCompare(b) * -1, []);
+// In this case there is no dependency since it do not take an external value
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,71 @@
-import { useState } from 'react';
+import { useState, useCallback, useMemo } from 'react';
+
+function SortedList({ list, sortFunc }) {
+  console.log('Using SortedList');
+  const sortedList = useMemo(() => {
+    console.log('Running sort');
+    return [...list].sort(sortFunc);
+  }, [list, sortFunc]);
+
+  return (
+    <div>
+      <h3>Sorted List : {sortedList.join(' , ')} </h3>
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const namesList = [
+    'sohan',
+    'akhil',
+    'manish',
+    'vishnu',
+    'vinit',
+    'abhilash',
+    'abhiram',
+    'ajinkya',
+    'shubham',
+  ];
 
-  return <div className='App'></div>;
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const totalCount = count1 + count2;
+
+  console.log(count1);
+  console.log(count2);
+
+  const sortFunc = useCallback((a, b) => a.localeCompare(b) * -1, []);
+  console.log('Reloading App ');
+
+  return (
+    <div className='App'>
+      <button
+        onClick={() => {
+          setCount1((prev) => (prev += 1));
+        }}
+      >
+        Count : {count1}
+      </button>
+      <button
+        onClick={() => {
+          setCount2((prev) => (prev += 1));
+        }}
+      >
+        Count : {count2}
+      </button>
+      <h3>Total Count : {totalCount}</h3>
+
+      <div>
+        <h3>Names : {namesList.join(' , ')}</h3>
+        <SortedList list={namesList} sortFunc={sortFunc} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
+useCallback(() => {
+  first;
+}, [second]);
