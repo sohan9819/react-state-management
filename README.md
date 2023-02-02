3244a148e49dba0a9dc77bcf68fcc34e16b82cd2
Sohan <sohanshetty2001@gmail.com>
2023-02-02 20:03:00 +0530

    Added code examples for useMemo

--- README.md
+++ README.md
@@ -11,3 +11,20 @@
 - Valtio
 - Jotal
 - Redux
+
+## `useMemo`
+
+[React Docs for useMemo](https://reactjs.org/docs/hooks-reference.html#usememo)
+
+- `useMemo()` is a memorization hook. Its generally used for avoid repeating the complex tasks and also for tasks related with objects and arrays .
+
+- Syntax
+
+```javascript
+//useMemo(() => first, [second])
+
+const [names] = useState(['sohan', 'arshit', 'manish', 'akhil']);
+
+const sortedNames = useMemo(() => [...names].sort(), [names]);
+// Returns a memoized value.
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  return <div className='App'></div>;
+  const total = useMemo(
+    () => numbers.reduce((sum, num) => (sum += num), 0),
+    [numbers]
+  );
+
+  const [names] = useState(['sohan', 'arshit', 'manish', 'akhil']);
+
+  const sortedNames = useMemo(() => [...names].sort(), [names]);
+
+  return (
+    <div className='App'>
+      <p>Total : {total}</p>
+      <div>
+        <p>Names : {names.join(' , ')}</p>
+        <p>Sorted Names : {sortedNames.join(' , ')}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
