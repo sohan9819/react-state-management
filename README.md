e19ff03a0414e0c470b9332243c855ce7a4a098a
Sohan <sohanshetty2001@gmail.com>
2023-02-04 21:11:53 +0530

    Added code examples for useRef

--- README.md
+++ README.md
@@ -11,3 +11,20 @@
 - Valtio
 - Jotal
 - Redux
+
+## `useRef`
+
+[React Docs for useRef](https://beta.reactjs.org/reference/react/useRef)
+
+```javascript
+// basic syntax
+// useRef(initialValue)
+const inputRef = useRef('');
+
+// In Html
+<input type='text' ref={inputRef} />;
+
+// Acccessing the value of the input filed in js
+inputElement = input.current; // will give the input element
+inputValue = input.current.value; // will give the input value
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([]);
 
-  return <div className='App'></div>;
+  const input = useRef(null);
+  const refId = useRef(users.length + 1);
+
+  useEffect(() => {
+    input.current.focus();
+  }, []);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value.trim();
+
+    if (name) {
+      setUsers((prev) => [...prev, { id: refId.current++, name }]);
+      e.target.name.value = '';
+    }
+  };
+
+  return (
+    <div className='App'>
+      <ul>
+        {users.map((user) => {
+          console.log(user);
+          return (
+            <li key={user.id}>
+              {user.id} - {user.name}
+            </li>
+          );
+        })}
+      </ul>
+      <form onSubmit={submitHandler}>
+        <input type='text' name='name' ref={input} />
+        <button type='submit'>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
